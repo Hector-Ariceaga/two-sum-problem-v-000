6051084d2b07670edbf55067550900527a92f70a
HECTOR ARICEAGA <hector_ariceaga@hotmail.com>
2019-05-14 21:40:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 let bruteForceTwoSum = (a, sum) => {
   let twoSums = [];
-  for (let i = 0; i < a.length; i++) {
-    for (let j = i +1 ; j < a.length; j++) {
-      if (sum - a[i] === a[j]) {
-        twoSums.push([a[i], a[j]])
+  if (a.length !== 0) {
+    for (let i = 0; i < a.length; i++) {
+      for (let j = i +1 ; j < a.length; j++) {
+        if (sum - a[i] === a[j]) {
+          twoSums.push([a[i], a[j]])
+        }
       }
-    }
-  } return twoSums
+    } return twoSums
+  }
+}
+
+
+let binarySearchTwoSum = (a, sum) => {
+  
 }
