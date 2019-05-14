700af28074eedfcd31196a8817fd01558ce920df
HECTOR ARICEAGA <hector_ariceaga@hotmail.com>
2019-05-14 21:01:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ let bruteForceTwoSum = (sum, a) => {
   for (let i = 0; i < a.length; i++) {
     for (let j = 0; i < a.length; j++) {
       if (a[i] != a[j]) {
-        if (sum === a[i] + a[j] && twoSums[twoSums.length - 1] != [a[i], a[j]]) {
-          return twoSums.push([a[i], a[j]])
+        if (sum === a[i] + a[j]) {
+          twoSums.push([a[i], a[j]])
         }
       }
     }
